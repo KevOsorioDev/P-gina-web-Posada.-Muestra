0458c3b0c062f0cb6ed7e23254bc365cd6f48baf
KevOsorioDev <kevin.osorio.dev@gmail.com>
2024-12-23 18:28:50 -0300

    deploy de github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mi-proyecto/', // Reemplaza "mi-proyecto" con el nombre exacto de tu repositorio
 })
