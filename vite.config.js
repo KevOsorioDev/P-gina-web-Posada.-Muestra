9cebcc697824b6365e8e95ca8ca2d497f7260f66
KevOsorioDev <kevin.osorio.dev@gmail.com>
2024-12-23 18:44:10 -0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-proyecto/', // Reemplaza "mi-proyecto" con el nombre exacto de tu repositorio
+  base: '/P-gina-web-Posada.-Muestra/',
 })
